5ce7b5b25e15627f9b27cbd0d17860d65182269a
vadwwxz <vadwwxz@gmail.com>
2023-04-26 21:29:38 +0300

    Updated build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,20 @@
 import com.android.build.api.dsl.Packaging
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id(Plugins.application)
+    id(Plugins.android)
 }
 
 android {
-    namespace = "com.main.voicevortex"
-    compileSdk = 33
+    namespace = Config.packageName
+    compileSdk = Config.compileSdk
 
     defaultConfig {
-        applicationId = "com.main.voicevortex"
-        minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        applicationId = Config.packageName
+        minSdk = Config.minSdk
+        targetSdk = Config.targetSdk
+        versionCode = Config.versionCode
+        versionName = Config.versionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
@@ -32,38 +32,42 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = Config.jvmTarget
     }
     @Suppress("UnstableApiUsage")
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = Dependencies.Compose.version
     }
-    fun Packaging.packaging() {
+    packagingOptions {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/gradle/incremental.annotation.processors"
         }
     }
 }
 
 dependencies {
     // Basic
-    implementation("androidx.core:core-ktx:1.10.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.1")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.1.0-rc01")
-    implementation("androidx.compose.material:material:1.4.2")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(Dependencies.Android.coreKtx)
+    implementation(Dependencies.Android.lifecycleKtx)
+    implementation(Dependencies.Android.activityCompose)
+    implementation(Dependencies.Compose.composeUi)
+    implementation(Dependencies.Compose.composeGraphics)
+    implementation(Dependencies.Compose.composePreviewTooling)
+    implementation(Dependencies.Compose.composeMaterial3)
+    implementation(Dependencies.Compose.composeMaterial)
+    testImplementation(Dependencies.Test.jUnit)
+    androidTestImplementation(Dependencies.Test.jUnitExt)
+    androidTestImplementation(Dependencies.Test.jUnitUi)
+    androidTestImplementation(Dependencies.Test.espresso)
+    debugImplementation(Dependencies.Compose.Debug.composeUiTooling)
+    debugImplementation(Dependencies.Compose.Debug.composeUiTestManifest)
     // Navigation
-    implementation("androidx.navigation:navigation-compose:2.6.0-beta01")
+    implementation(Dependencies.Navigation.navigationCompose)
+    // Hilt
+    implementation(Dependencies.Hilt.android)
+    implementation(Dependencies.Hilt.compiler)
+    implementation(Dependencies.Hilt.navigation)
 }
